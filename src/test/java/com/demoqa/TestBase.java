56101e578d5165fac67f05873182fb094323b366
AGordey <gordey.andrej@gmail.com>
2022-05-01 17:45:43 +0300

    First

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -13,15 +13,15 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class TestBase {
 
 
-    String base_url = System.getProperty("base_url","https://demoqa.tt");
-    String browser_size = System.getProperty("browser_size","1920x1080");
+//    String base_url = System.getProperty("base_url","https://demoqa.tt");
+//    String browser_size = System.getProperty("browser_size","1920x1080");
 
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.baseUrl = base_url ;
-        Configuration.browserSize = browser_size;
+        Configuration.baseUrl = System.getProperty("base_url","https://demoqa.tt");
+        Configuration.browserSize = System.getProperty("browser_size","1920x1080");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
